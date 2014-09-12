416fca74972e7e2b68876cf4f2f86eb33cef4a0e
shigh <highscot@gmail.com>
2014-09-12 14:30:59 -0500

    Added weight function

--- py3d3v/interp.pyx
+++ py3d3v/interp.pyx
@@ -39,3 +39,38 @@ cpdef np.ndarray[DOUBLE,ndim=1] TriLinearInterp(np.ndarray[DOUBLE,ndim=3] vals,
         c[i] = c0*(1.-zd) + c1*zd
 
     return c
+
+
+cpdef weight_cic(np.ndarray[DOUBLE,ndim=3] grid,
+                 np.ndarray[DOUBLE,ndim=1] z,
+                 np.ndarray[DOUBLE,ndim=1] y,
+                 np.ndarray[DOUBLE,ndim=1] x,
+                 np.ndarray[DOUBLE,ndim=1] q,
+                 double rho0=0.):
+    """Assume all inputs are scaled to grid points
+    
+    Periodic in x, y, z
+    """
+    cdef int i
+    cdef int N = z.shape[0]
+    cdef int nz, ny, nx
+    cdef double xd, yd, zd, qi
+    cdef double x0, x1, y0, y1, z0, z1
+    nz, ny, nx = grid.shape[0], grid.shape[1], grid.shape[2]
+    
+    grid[:,:,:] = rho0
+    for i in range(N):
+        x0, x1 = floor(x[i]), ceil(x[i])%nx
+        y0, y1 = floor(y[i]), ceil(y[i])%ny
+        z0, z1 = floor(z[i]), ceil(z[i])%nz
+        xd, yd, zd = (x[i]-x0), (y[i]-y0), (z[i]-z0)
+        qi = q[i]
+        
+        grid[z0, y0, x0] += qi*(1-xd)*(1-yd)*(1-zd)
+        grid[z0, y0, x1] += qi*xd*(1-yd)*(1-zd)
+        grid[z0, y1, x0] += qi*(1-xd)*yd*(1-zd)
+        grid[z0, y1, x1] += qi*xd*yd*(1-zd)
+        grid[z1, y0, x0] += qi*(1-xd)*(1-yd)*zd
+        grid[z1, y0, x1] += qi*xd*(1-yd)*zd
+        grid[z1, y1, x0] += qi*(1-xd)*yd*zd
+        grid[z1, y1, x1] += qi*xd*yd*zd    
--- py3d3v/test_interp.py
+++ py3d3v/test_interp.py
@@ -6,6 +6,8 @@ norm = lambda x: np.max(np.abs(x))
 
 class TestInterp(unittest.TestCase):
 
+    tol = 10e-10
+
     def test_interior_points(self):
         
         nz, ny, nx = 10, 20, 30
@@ -25,4 +27,85 @@ class TestInterp(unittest.TestCase):
 
         c = TriLinearInterp(vals, z_vals/dz, y_vals/dy, x_vals/dx)
 
-        self.assertTrue( norm(c-expected)<10e-10 )
+        self.assertTrue( norm(c-expected) < self.tol )
+
+
+class TestWeight(unittest.TestCase):
+
+    tol = 10e-10
+
+    def test_interior_points(self):
+        nz, ny, nx = 2, 2, 2
+        grid = np.zeros((nz, ny, nx))
+        L = 1.
+        dz, dy, dx = L/nz, L/ny, L/nx
+
+        x_vals = np.array([.25])
+        y_vals = np.array([.25])
+        z_vals = np.array([.25])
+        q_vals = np.ones_like(z_vals)
+
+        weight_cic(grid, z_vals/dz, y_vals/dy, x_vals/dx, q_vals)
+
+        self.assertTrue( (grid.flatten()-0.125<self.tol).all() )
+        self.assertTrue( np.sum(grid)-1.0 < self.tol )
+
+    def test_exterior_points(self):
+        nz, ny, nx = 2, 2, 2
+        grid = np.zeros((nz, ny, nx))
+        L = 1.
+        dz, dy, dx = L/nz, L/ny, L/nx
+
+        x_vals = np.array([.75])
+        y_vals = np.array([.75])
+        z_vals = np.array([.75])
+        q_vals = np.ones_like(z_vals)
+
+        weight_cic(grid, z_vals/dz, y_vals/dy, x_vals/dx, q_vals)
+
+        self.assertTrue( (grid.flatten()-0.125 < self.tol).all() )
+        self.assertTrue( np.sum(grid)-1.0 < self.tol )
+
+    def test_on_grid_point(self):
+        nz, ny, nx = 2, 2, 2
+        grid = np.zeros((nz, ny, nx))
+        L = 1.
+        dz, dy, dx = L/nz, L/ny, L/nx
+
+        x_vals = np.array([.5])
+        y_vals = np.array([.5])
+        z_vals = np.array([.5])
+        q_vals = np.ones_like(z_vals)
+
+        weight_cic(grid, z_vals/dz, y_vals/dy, x_vals/dx, q_vals)
+        expected = np.zeros_like(grid)
+        expected[1,1,1] = 1.
+
+        self.assertTrue( (grid-expected < self.tol).all() )
+        self.assertTrue( np.sum(grid)-1.0 < self.tol )
+
+    def test_multiple_points(self):
+        nz, ny, nx = 2, 2, 2
+        grid = np.zeros((nz, ny, nx))
+        L = 1.
+        dz, dy, dx = L/nz, L/ny, L/nx
+
+        x_vals = np.array([.1, .2])
+        y_vals = np.array([.1, .2])
+        z_vals = np.array([.1, .2])
+        q_vals = np.ones_like(z_vals)
+
+        weight_cic(grid, z_vals/dz, y_vals/dy, x_vals/dx, q_vals)
+        expected = np.zeros_like(grid)
+        expected[0,0,0] = .8**3
+        for z in [0,1]:
+            for y in [0,1]:
+                for x in [0,1]:
+                    s = z+y+x
+                    expected[z,y,x] = .8**(3-s)*.2**s \
+                                    + .6**(3-s)*.4**s
+
+        self.assertTrue( norm(grid-expected) < self.tol )
+        self.assertTrue( np.abs(np.sum(grid)-2.0) < self.tol )
+
+
