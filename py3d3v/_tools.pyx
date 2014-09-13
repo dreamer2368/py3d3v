c54114e41dffa25caf61a6206909fc267b8da804
shigh <highscot@gmail.com>
2014-09-13 15:36:23 -0500

    Optimized build_k2.
    Went 6ish seconds to less than a second for the entire __init__ routine in Solver3DFFT

--- py3d3v/_tools.pyx
+++ py3d3v/_tools.pyx
@@ -7,19 +7,22 @@ cpdef build_k2(int nz, double dz,
                int ny, double dy,
                int nx, double dx):
 
-    cdef np.ndarray kz = np.fft.fftfreq(nz)*2*np.pi/dz
-    cdef np.ndarray ky = np.fft.fftfreq(ny)*2*np.pi/dy
-    cdef np.ndarray kx = np.fft.fftfreq(nx)*2*np.pi/dx
+    cdef np.ndarray kz2 = (np.fft.fftfreq(nz)*2*np.pi/dz)**2
+    cdef np.ndarray ky2 = (np.fft.fftfreq(ny)*2*np.pi/dy)**2
+    cdef np.ndarray kx2 = (np.fft.fftfreq(nx)*2*np.pi/dx)**2
+    cdef double kz2i, ky2i
 
     cdef int nkz, nky, nkx
     cdef int iz, iy, ix
     
-    nkz, nky, nkx = len(kz), len(ky), len(kx)
+    nkz, nky, nkx = len(kz2), len(ky2), len(kx2)
     cdef np.ndarray k2_vals = np.zeros((nkz, nky, nkx), dtype=np.double)
     for iz in range(nkz):
+        kz2i = kz2[iz]
         for iy in range(nky):
+            ky2i = ky2[iy]
             for ix in range(nkx):
-                k2_vals[iz,iy,ix] = kz[iz]**2+ky[iy]**2+kx[ix]**2
+                k2_vals[iz,iy,ix] = kz2i+ky2i+kx2[ix]
     # Avoid a divide by zero            
     k2_vals[0,0,0] = 1.
 
