2227f8673cd7add037b250c5bcbc73102ba38173
Kevin SeungWhan Chung <schung58@illinois.edu>
2016-01-13 21:56:02 -0600

    personal setting

---
+++ py3d3v/setup-pic.sh
@@ -0,0 +1,5 @@
+#!/bin/bash --login
+
+export CXX=/Users/Kevin/bin/gcc/bin/g++
+export CC=/Users/Kevin/bin/gcc/bin/g++
+export PYTHONPATH=/Users/Kevin/Desktop/XPACC/ParticleInCell/3DPIC/py3d3v/py3d3v
--- py3d3v/setup.py
+++ py3d3v/setup.py
@@ -3,25 +3,28 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy as np
 
+eca = ['-fopenmp', '-arch', 'x86_64']
+ela = eca
+
 setup(
     cmdclass = {'build_ext':build_ext},
     include_dirs = [np.get_include()],
     ext_modules = [Extension("interp",["interp.pyx", "par_interp.cpp"],
                              libraries=["m"],
-                             extra_compile_args=['-fopenmp'],
-                             extra_link_args=['-fopenmp'],
+                             extra_compile_args=eca,
+                             extra_link_args=ela,
                              language="c++"),
                    Extension("ewald",["ewald.pyx"],
                              libraries=["m"],
-                             extra_compile_args=['-fopenmp'],
-                             extra_link_args=['-fopenmp'],
+                             extra_compile_args=eca,
+                             extra_link_args=ela,
                              language="c++"),
                    Extension("core",["core.pyx", "par_core.cpp"],
-                             extra_compile_args=['-fopenmp'],
-                             extra_link_args=['-fopenmp'],
+                             extra_compile_args=eca,
+                             extra_link_args=ela,
                              language="c++"),
                    Extension("solvers",["solvers.pyx", "par_solvers.cpp"],
-                             extra_compile_args=['-fopenmp'],
-                             extra_link_args=['-fopenmp'],
+                             extra_compile_args=eca,
+                             extra_link_args=ela,
                              language="c++")]
 )
